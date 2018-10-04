73d12ead814dae4704601f9dc8ec7ffee019fe1e
oda-yoshikazu <oda.yoshikazu@dena.jp>
2018-10-04 11:00:36 +0900

    Initial Commit

---
+++ demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Yoshikazu Oda on 2018/10/04.
+//  Copyright © 2018年 yoshinbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
